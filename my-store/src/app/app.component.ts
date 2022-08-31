3feacc484a550a2ff68799388ef5953eb0731c0c
anna_tama <anna.tama.roma@gmail.com>
2022-08-31 15:47:36 -0300

    init: *ngFor

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -16,6 +16,9 @@ export class AppComponent {
    ,avatar : 'https://source.unsplash.com/random'
   }
 
+  names: string[] = ['Anna', 'Tama', 'Roma']
+  newName = '';
+
   toggleButton(){
     this.btnDisabled = !this.btnDisabled;
   }
@@ -32,5 +35,14 @@ export class AppComponent {
   changeName(event: Event){
     const element = event.target as HTMLInputElement;
     this.person.name = element.value;
+  }
+
+  addName(){
+    this.names.push(this.newName);
+    this.newName = '';
+  }
+
+  deleteName(index: number){
+    this.names.splice(index,1)
   }
 }
